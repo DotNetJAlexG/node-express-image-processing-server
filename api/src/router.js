7226440efbf2ba65d8891d439f7e262081c74e06
Joe Geniec <jgeniec@yahoo.com>
2021-06-01 05:28:27 -0400

    Commit 5

--- api/src/router.js
+++ api/src/router.js
@@ -1,13 +1,10 @@
 const {Router} = require('express');
 const multer = require('multer');
 
-const path = require('path');
-const imageProcessor = require('./imageProcessor');
-
 const router = Router();
 
 const filename = (request, file, callback) => {
-    callback(null, file.originalName);
+    callback(null, file.originalname);
 }
 
 const storage = multer.diskStorage({
