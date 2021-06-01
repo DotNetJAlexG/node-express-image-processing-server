8c05115110d6446a5365a1427aabd1ed8586efdc
Joe Geniec <jgeniec@yahoo.com>
2021-06-01 05:25:07 -0400

    COmmit 4

--- api/src/router.js
+++ api/src/router.js
@@ -1,18 +1,21 @@
 const {Router} = require('express');
 const multer = require('multer');
+
+const path = require('path');
+const imageProcessor = require('./imageProcessor');
+
 const router = Router();
+
+const filename = (request, file, callback) => {
+    callback(null, file.originalName);
+}
+
 const storage = multer.diskStorage({
     destination: 'api/uploads/', 
     filename,
 });
-const upload = multer({fileFilter: fileFilter, storage: storage});
 
-
-function filename(request, file, callback) {
-    callback(null, file.originalName);
-}
-
-function fileFilter(request, file, callback) {
+const fileFilter = (request, file, callback) => {
     if (file.mimetype !== 'image/png') {
         request.fileValidationError = 'Wrong file type';
         callback(null, false, new Error('Wrong file type'));
@@ -21,6 +24,8 @@ function fileFilter(request, file, callback) {
         callback(null, true);
     }
 }
+
+const upload = multer({fileFilter, storage});
 
 router.post('/upload', upload.single('photo'), (request, response) => {
     if ('fileValidationError' in request) {
