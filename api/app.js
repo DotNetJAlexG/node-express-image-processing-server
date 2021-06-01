928ac0e9b088e600a96d32030fe37e3bcf3eb83e
Joe Geniec <jgeniec@yahoo.com>
2021-05-31 23:12:58 -0400

    module 2 step 1

--- api/app.js
+++ api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const router = require('./src/router');
+
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
-
+app.use('/');
 app.use('/*', (request, response) => { response.sendFile(pathToIndex); });
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 module.exports = app;
